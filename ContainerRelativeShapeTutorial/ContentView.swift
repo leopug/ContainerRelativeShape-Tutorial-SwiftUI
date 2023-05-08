be200e5d03d88cb6309de6a94b1413e25c88bf7b
Leonardo Pugliese <leonardo.silva@impalastudios.com>
2023-05-08 13:21:59 +0200

    moving back the code

--- ContainerRelativeShapeTutorial/ContentView.swift
+++ ContainerRelativeShapeTutorial/ContentView.swift
@@ -9,43 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
-        ZStack {
-            ContainerRelativeShape()
-                .inset(by: 20)
-                .fill(Color.red)
-            
-        }.ignoresSafeArea()
-        
-//        HStack {
-//            Text("Example 1")
-//                .bold()
-//                .padding(22)
-//                .background(.red, in: RoundedRectangle(cornerRadius: 30))
-//                .padding(5)
-//                .background(.black, in: RoundedRectangle(cornerRadius: 30))
-//                .padding(5)
-//                .background(.green, in: RoundedRectangle(cornerRadius: 30))
-//                .padding(5)
-//                .background(.yellow, in: RoundedRectangle(cornerRadius: 30))
-//                .padding(5)
-//                .background(.blue, in: RoundedRectangle(cornerRadius: 30))
-//
-//            Text("Example 2")
-//                .bold()
-//                .padding(22)
-//                .background(ContainerRelativeShape().fill(Color.red))
-//                .padding(5)
-//                .background(ContainerRelativeShape().fill(Color.black))
-//                .padding(5)
-//                .background(ContainerRelativeShape().fill(Color.green))
-//                .padding(5)
-//                .background(ContainerRelativeShape().fill(Color.yellow))
-//                .padding(5)
-//                .background(ContainerRelativeShape().fill(Color.blue))
-//                .background(.clear, in: RoundedRectangle(cornerRadius: 30))
-//        }
-//        .padding()
+        HStack {
+            Text("Example 1")
+                .bold()
+                .padding(22)
+                .background(.red, in: RoundedRectangle(cornerRadius: 30))
+                .padding(5)
+                .background(.black, in: RoundedRectangle(cornerRadius: 30))
+                .padding(5)
+                .background(.green, in: RoundedRectangle(cornerRadius: 30))
+                .padding(5)
+                .background(.yellow, in: RoundedRectangle(cornerRadius: 30))
+                .padding(5)
+                .background(.blue, in: RoundedRectangle(cornerRadius: 30))
+
+            Text("Example 2")
+                .bold()
+                .padding(22)
+                .background(ContainerRelativeShape().fill(Color.red))
+                .padding(5)
+                .background(ContainerRelativeShape().fill(Color.black))
+                .padding(5)
+                .background(ContainerRelativeShape().fill(Color.green))
+                .padding(5)
+                .background(ContainerRelativeShape().fill(Color.yellow))
+                .padding(5)
+                .background(ContainerRelativeShape().fill(Color.blue))
+                .background(.clear, in: RoundedRectangle(cornerRadius: 30))
+        }
+        .padding()
     }
 }
 
